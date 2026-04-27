9a6155502fd0382f4e14ab842c9dd805953c62ea
Enzo Dias <enzocdias2008@gmail.com>
2026-04-26 22:45:15 -0300

    Adicionando API de principais vulnerabilidades de acordo com o protocolo e versão do banner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ##Esse arquivo roda um Port Scanner (Retorna as portas abertas dentro de um range) e um banner grabber (Retorna o banner de identificação das portas abertas) e gera um relatório com as principais informações das portas.
 
-import socket, json
+import socket, json, requests
 
 ip = input("Digite o IP:  ")
 porta1 = int(input("Digite a primeira porta a ser varrida: "))
@@ -8,6 +8,7 @@ porta2 = int(input("Digite a última porta a ser varrida: "))
 portas_abertas = []
 bloqueadas = []
 fechadas = []
+
 
 for porta in range(porta1, porta2 + 1):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #cria socket IPV4, TCP/IP
@@ -18,7 +19,21 @@ for porta in range(porta1, porta2 + 1):
             banner = s.recv(1024).decode(errors="ignore")
         except socket.timeout:
             banner = "sem banner"
-        portas_abertas.append({"porta": porta, "banner": banner})
+        vulnerabilidades = []
+        if banner != "sem banner":
+            servico = banner.split()[0].split("-")[0]
+            api_url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch={servico}"
+            try:
+                resposta = requests.get(api_url)
+                dados = resposta.json()
+                if len(dados["vulnerabilities"]) > 0:                
+                    for item in dados["vulnerabilities"]:
+                        id_cve = item["cve"]["id"]
+                        descricao = item["cve"]["descriptions"][0]["value"]
+                        vulnerabilidades.append({"id": id_cve, "descricao": descricao})
+            except:
+                pass
+        portas_abertas.append({"porta": porta, "banner": banner, "vulnerabilidades": vulnerabilidades})
     except socket.timeout:
         bloqueadas.append(porta)
     except socket.error:
